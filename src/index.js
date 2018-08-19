d465bd03792b8fc1aacc332baa1c64047cc0587f
GMaiolo <guille.maiolo@gmail.com>
2018-08-19 17:31:11 -0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const routes = require('./routes')
 const secret = require('./secret')
 
 // setup
 const port = 3000
 const app = express()
+
 app.use(bodyParser.json())
+app.use(cors())
 mongoose.connect(secret.URI, { useNewUrlParser: true })
+
+app.use((req, res, next) => {
+  req.headers['Access-Control-Allow-Origin'] = '*'
+  next()
+})
 
 // routing
 app.post('/expenses', routes.expenses.post)
