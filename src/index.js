b6957eac2228558486588e63d71400b14cf2f589
GMaiolo <guille.maiolo@gmail.com>
2018-09-09 15:19:59 -0300

    adaptations for be

--- src/index.js
+++ src/index.js
@@ -6,17 +6,11 @@ const routes = require('./routes')
 const secret = require('./secret')
 
 // setup
-const port = 3000
+const port = 8086
 const app = express()
-
 app.use(bodyParser.json())
 app.use(cors())
 mongoose.connect(secret.URI, { useNewUrlParser: true })
-
-app.use((req, res, next) => {
-  req.headers['Access-Control-Allow-Origin'] = '*'
-  next()
-})
 
 // routing
 app.post('/expenses', routes.expenses.post)
